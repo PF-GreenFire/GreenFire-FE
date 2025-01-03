db7f99cd8d50e3476c8f93970e17b674b315e3ce
sisology <gtldhs120@gmail.com>
2025-01-03 13:17:22 +0900

    fix: 누락 import 추가 및 npm intall react-redux 실행

--- src/store.js
+++ src/store.js
@@ -1,5 +1,7 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from "./modules/root";
+import { thunk } from 'redux-thunk';
+import logger from 'redux-logger';
 
 const store =  createStore(
     rootReducer,
